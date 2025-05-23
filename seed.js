003792af62023401ae357cc5df15c476fb0af58c
ellipti <108758165+ellipti@users.noreply.github.com>
2025-05-23 19:12:00 +0800

    s

--- seed.js
+++ seed.js
@@ -17,15 +17,85 @@ mongoose.connect(process.env.MONGODB_URI)
     // Админ хэрэглэгч үүсгэх
     await User.create({
       name: 'Админ',
-      username: 'admin',
-      password: '1234',
+      username: 'zolboo',
+      password: 'zolboo9911',
       role: 'admin'
     });
 
     await User.create({
       name: 'Hereglegch',
-      username: 'user',
+      username: 'user1',
       password: '1234',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user1',
+      password: '9900',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user2',
+      password: '2211',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user3',
+      password: '8808',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user4',
+      password: '9515',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user5',
+      password: '9969',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user6',
+      password: '4321',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user7',
+      password: '5678',
+      role: 'user'
+    });
+
+    await User.create({
+      name: 'Hereglegch',
+      username: 'user8',
+      password: '9876',
+      role: 'user'
+    });
+
+     await User.create({
+      name: 'Hereglegch',
+      username: 'user9',
+      password: '6543',
+      role: 'user'
+    });
+
+     await User.create({
+      name: 'Hereglegch',
+      username: 'user10',
+      password: '0987',
       role: 'user'
     });
 
